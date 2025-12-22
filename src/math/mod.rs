609dc2ac959317e0a9de845c7ffa4ac7cc8a7830
Eloi Dieme <eloidieme@gmail.com>
2025-12-22 17:12:28 +0100

    minor change

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -2,7 +2,6 @@ pub mod interval;
 pub mod ray;
 pub mod vec3;
 
-// Re-export common types so you can use `crate::math::Vec3` directly
 pub use interval::Interval;
 pub use ray::Ray;
 pub use vec3::{Color, Point3, Vec3};
